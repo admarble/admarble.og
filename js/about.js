3edbb4a96db01b518e0fef5358c1688eb0f2244b
Antonio <admarble@gmail.com>
2020-08-17 14:18:36 -0500

    Added loading screen to about page

--- js/about.js
+++ js/about.js
@@ -4,7 +4,8 @@
 // gsap.from(".branding", {immediateRender: true, smoothOrigin:true, duration: 1, scale: 1.2, ease: "circ.out"});
 
 var tl = gsap.timeline({defaults:{ease: "circ.out", opacity: 0, autoAlpha: 0}})
-tl.from(".am-profile", {immediateRender: true, smoothOrigin:true, opacity: 0, duration:4, y: 150, duration:2, ease: "circ.out"})
+tl.to("#loading-screen2", {immediateRender: true, smoothOrigin:true, delay: 1, duration: 1})
+.from(".am-profile", {immediateRender: true, smoothOrigin:true, opacity: 0, duration:4, y: 150, duration:2, ease: "circ.out"})
 .from(".branding", {immediateRender: true, smoothOrigin:true, duration: 3, scale: 1.2, ease: "circ.out"},"-=1.2")
 
 // document.getElementById('cta').addEventListener('click',() => {
