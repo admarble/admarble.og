e250fb53ff2656f1db9668b80058b4908ede9786
Antonio <admarble@gmail.com>
2020-08-27 00:07:11 -0500

    Made loading screen active

--- js/playground.js
+++ js/playground.js
@@ -1,7 +1,7 @@
-// var tl = gsap.timeline({defaults:{duration: 1, ease: "circ.out", opacity: 0, autoAlpha: 0}})
-// tl.to("#loading-screen", {immediateRender: true, smoothOrigin:true, delay: 1, duration: 1})
-// .from(".carousel", {immediateRender: true, smoothOrigin:true, duration: 1, y: 150, stagger: 0.2})
-// .from(".branding", {immediateRender: true, smoothOrigin:true, duration: 3, scale: 1.2},"-=1.2")
+var tl = gsap.timeline({defaults:{duration: 1, ease: "circ.out", opacity: 0, autoAlpha: 0}})
+tl.to("#loading-screen", {immediateRender: true, smoothOrigin:true, delay: 1, duration: 1})
+.from(".carousel", {immediateRender: true, smoothOrigin:true, duration: 1, y: 150, stagger: 0.2})
+.from(".branding", {immediateRender: true, smoothOrigin:true, duration: 3, scale: 1.2},"-=1.2")
 
 var tl2 = gsap.timeline({defaults:{}})
 tl2.to(".loading-letter", {immediateRender: true, smoothOrigin:true, y: 20, skewX:45, stagger: 0.1, repeat: -1, repeatDelay:0.2, yoyo: true})
